4c9daa752788ab1f667e0fdafee07f008753021f
Ognian Deianov <44973718+Deianov@users.noreply.github.com>
2024-03-14 21:20:53 +0100

    change decoding function

--- private/index.js
+++ private/index.js
@@ -1,9 +1,10 @@
-const decode = (s) => decodeURIComponent(escape(window.atob(s)));
 const getId = (id) => document.getElementById(id);
-const numbers1 = [0,0,0,0, "", 18.24, 1.90, 13.68, 7.07, 2.48, 7.85, 4.10, 8.61];
-const numbers2 = [0,0, 7.00, 3.29, 9.63, 6.73];
-const titles = decode("0LDQv9Cw0YDRgtCw0LzQtdC90YI7CtGC0LDQstCw0L07CtC60LDRgNGC0LA7CtGD0LsuINCb0Y7QsdC10L0g0JrQsNGA0LDQstC10LvQvtCyIOKEliA1LdCwLCDQtdGCLiA2LCDQsNC/LiAyMTs=").split(";");
-const labels = decode("NzcuMzkg0LrQsi7QvCAtINCX0LDRgdGC0YDQvtC10L3QsCDQv9C70L7RiSAo0JfQnyk7NjcuNTUg0LrQsi7QvCAtINC/0L4g0J3QkCwg0LfQsNC10LTQvdC+INGBIDQuNzElINC40LQu0YcuINC+0YIg0YHQs9GA0LDQtNCw0YLQsCDQuCDQvNGP0YHRgtC+0YLQvjs2My45MyDQutCyLtC8IC0g0L/Qu9C+0YnQuDt+My4wMCDQutCyLtC8IC0g0YHQutC70LDQtNC+0LLQviDQv9GA0L7RgdGC0YDQsNC90YHRgtCy0L4gSD02MCDRgdC8OzvQtNC90LXQstC90LA70YHRgtGK0LvQsdC4O9GB0L/QsNC70L3RjzvRgtC10YDQsNGB0LA70LHQsNC70LrQvtC9O9C60YPRhdC90Y870LHQsNC90Y87OzI2LjY1INC60LIu0LwgLSDRh9C40YHRgtCwINC/0LvQvtGJO9Cy0LjRgdC+0YfQuNC90LA6IDEwMy0xODUg0YHQvC470YHQutC70LDQtDvRgdC60LvQsNC0O9GB0L/QsNC70L3Rjzs=").split(";");
+const numbers1 = [0, 0, 0, 0, "", 18.24, 1.9, 13.68, 7.07, 2.48, 7.85, 4.1, 8.61];
+const numbers2 = [0, 0, 7.0, 3.29, 9.63, 6.73];
+const titles = toUTF8("0LDQv9Cw0YDRgtCw0LzQtdC90YI7CtGC0LDQstCw0L07CtC60LDRgNGC0LA7CtGD0LsuINCb0Y7QsdC10L0g0JrQsNGA0LDQstC10LvQvtCyIOKEliA1LdCwLCDQtdGCLiA2LCDQsNC/LiAyMTs=").split(";");
+const labels = toUTF8(
+	"NzcuMzkg0LrQsi7QvCAtINCX0LDRgdGC0YDQvtC10L3QsCDQv9C70L7RiSAo0JfQnyk7NjcuNTUg0LrQsi7QvCAtINC/0L4g0J3QkCwg0LfQsNC10LTQvdC+INGBIDQuNzElINC40LQu0YcuINC+0YIg0YHQs9GA0LDQtNCw0YLQsCDQuCDQvNGP0YHRgtC+0YLQvjs2My45MyDQutCyLtC8IC0g0L/Qu9C+0YnQuDt+My4wMCDQutCyLtC8IC0g0YHQutC70LDQtNC+0LLQviDQv9GA0L7RgdGC0YDQsNC90YHRgtCy0L4gSD02MCDRgdC8OzvQtNC90LXQstC90LA70YHRgtGK0LvQsdC4O9GB0L/QsNC70L3RjzvRgtC10YDQsNGB0LA70LHQsNC70LrQvtC9O9C60YPRhdC90Y870LHQsNC90Y87OzI2LjY1INC60LIu0LwgLSDRh9C40YHRgtCwINC/0LvQvtGJO9Cy0LjRgdC+0YfQuNC90LA6IDEwMy0xODUg0YHQvC470YHQutC70LDQtDvRgdC60LvQsNC0O9GB0L/QsNC70L3Rjzs="
+).split(";");
 const content = getId("content");
 
 const contentHome = content.innerHTML;
@@ -12,78 +13,87 @@ const contents = [contentHome, contentHome, contentMap];
 
 /* menu */
 const links = document.getElementsByClassName("menu")[0].getElementsByTagName("a");
-for (let i = 0; i < linksMenu.length; i++) {
-    const a = linksMenu[i];
-    // a.textContent = titles[i];
-    a.addEventListener("click", function() {
-        content.innerHTML = contents[i];
-        updateDom(i);
-    })
-};
+for (let i = 0; i < links.length; i++) {
+	const a = links[i];
+	// a.textContent = titles[i];
+	a.addEventListener("click", function () {
+		content.innerHTML = contents[i];
+		updateDom(i);
+	});
+}
 
 function updateDom(index) {
-    if(index === 2) {
-        return;
-    }
-    /* set title */
-    content.getElementsByClassName("title")[0].innerHTML = `<span>${titles[3]}</span>`;
-    /* set level */
-    getId("level1").classList.toggle("none", index ==! 0);
-    getId("level2").classList.toggle("none", index ==! 1);
-    /* add events */
-    addButtons();
-    /* hide inactive layers */
-    setLayer(layers[currentLayer]);
+	if (index === 2) {
+		return;
+	}
+	/* set title */
+	content.getElementsByClassName("title")[0].innerHTML = `<span>${titles[3]}</span>`;
+	/* set level */
+	getId("level1").classList.toggle("none", index == !0);
+	getId("level2").classList.toggle("none", index == !1);
+	/* add events */
+	addButtons();
+	/* hide inactive layers */
+	setLayer(layers[currentLayer]);
 }
 
 /* toggle layers */
-
 let currentLayer = 0;
 const layers = ["white", "zp", "gr"];
 
 function addButtons() {
-    const buttons = document.getElementsByClassName("buttons")[0].children;
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener("click", function() {
-            setLayer(layers[i]);
-        })
-    }
+	const buttons = document.getElementsByClassName("buttons")[0].children;
+	for (let i = 0; i < buttons.length; i++) {
+		buttons[i].addEventListener("click", function () {
+			setLayer(layers[i]);
+		});
+	}
 }
 
 function setButton(name) {
-    const buttons = document.getElementsByClassName("buttons")[0].children;
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].classList.toggle("active", buttons[i].classList.contains(name));
-    }
+	const buttons = document.getElementsByClassName("buttons")[0].children;
+	for (let i = 0; i < buttons.length; i++) {
+		buttons[i].classList.toggle("active", buttons[i].classList.contains(name));
+	}
 }
 
 function setLayer(name) {
-    currentLayer = layers.indexOf(name);
-    const gs = document.getElementsByClassName("toggle");
-    for (let c = 0; c < gs.length; c++) {
-        const g = gs[c];
-        const elements = g.children;
+	currentLayer = layers.indexOf(name);
+	const gs = document.getElementsByClassName("toggle");
+	for (let c = 0; c < gs.length; c++) {
+		const g = gs[c];
+		const elements = g.children;
 
-        for (let i = 0; i < elements.length; i++) {
-            const e = elements[i];
-            e.classList.toggle("none", !e.classList.contains(name));
-
-        }
-    }
-    label(content.getElementsByTagName("text"), 0, labels);
-    // show areas
-    if (currentLayer === 2) {
-        label(getId("level1").getElementsByTagName("text"), 4, numbers1);
-        label(getId("level2").getElementsByTagName("text"), 2, numbers2);
-    }
-    setButton(name);
+		for (let i = 0; i < elements.length; i++) {
+			const e = elements[i];
+			e.classList.toggle("none", !e.classList.contains(name));
+		}
+	}
+	label(content.getElementsByTagName("text"), 0, labels);
+	// show areas
+	if (currentLayer === 2) {
+		label(getId("level1").getElementsByTagName("text"), 4, numbers1);
+		label(getId("level2").getElementsByTagName("text"), 2, numbers2);
+	}
+	setButton(name);
 }
 
 function label(points, fromIndex, text) {
-    for (let i = fromIndex; i < points.length && i < text.length; i++) {
-        const v = text[i];
-        points[i].textContent = isNaN(v) || v === "" ? v : Number(v).toFixed(2);
-    }
+	for (let i = fromIndex; i < points.length && i < text.length; i++) {
+		const v = text[i];
+		points[i].textContent = isNaN(v) || v === "" ? v : Number(v).toFixed(2);
+	}
+}
+
+function toUTF8(base64String) {
+	const binaryString = atob(base64String);
+	const uint8Array = new Uint8Array(binaryString.length);
+
+	for (let i = 0; i < binaryString.length; i++) {
+		uint8Array[i] = binaryString.charCodeAt(i);
+	}
+	const utf8Decoded = new TextDecoder().decode(uint8Array);
+	return utf8Decoded;
 }
 
 updateDom(0);
