afabd0245ab79c764d2f58eac36cfc470104b932
Ognian Deianov <44973718+Deianov@users.noreply.github.com>
2024-04-21 13:24:19 +0200

    Update projects/routes

--- src/app/features/projects/projects.component.ts
+++ src/app/features/projects/projects.component.ts
@@ -1,19 +1,20 @@
-import { AsyncPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import {AsyncPipe} from '@angular/common';
+import {Component, inject} from '@angular/core';
+import {RouterLink} from '@angular/router';
 
-import { DataService } from '../../core/services/data.service';
+import {DataService} from '../../core/services/data.service';
 
 @Component({
-  selector: 'app-projects',
-  standalone: true,
-  imports: [AsyncPipe],
-  templateUrl: './projects.component.html',
-  styleUrl: './projects.component.css',
+    selector: 'app-projects',
+    standalone: true,
+    imports: [AsyncPipe, RouterLink],
+    templateUrl: './projects.component.html',
+    styleUrl: './projects.component.css',
 })
 export class ProjectsComponent {
-  projects = inject(DataService).getProjects();
+    projects = inject(DataService).getProjects();
 
-  removeHttps(str: string) {
-    return str.replace('https://', '');
-  }
+    removeHttps(str: string) {
+        return str.replace('https://', '');
+    }
 }
