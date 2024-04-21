50acd045769ee23b7d35061d138d6d71b1c74e97
Ognian Deianov <44973718+Deianov@users.noreply.github.com>
2024-04-21 21:43:11 +0200

    Update mobile detection

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,33 +1,33 @@
-import { Component, inject, model, ModelSignal } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-import { MobileService } from '@core/services/mobile.service';
-import { ThemeService } from '@core/services/theme.service';
+import {Component, inject, model, ModelSignal} from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+import {MobileService} from '@core/services/mobile.service';
+import {ThemeService} from '@core/services/theme.service';
 
-import { NavComponent } from '../nav/nav.component';
+import {NavComponent} from '../nav/nav.component';
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [RouterLink, RouterLinkActive, NavComponent],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css',
+    selector: 'app-header',
+    standalone: true,
+    imports: [RouterLink, RouterLinkActive, NavComponent],
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-  private themeService = inject(ThemeService);
-  private mobileService = inject(MobileService);
+    private themeService = inject(ThemeService);
+    private mobileService = inject(MobileService);
 
-  theme: ModelSignal<string> = model.required<string>();
+    theme: ModelSignal<string> = model.required<string>();
 
-  showMenu = true;
-  showMenuTrigger = false;
+    showMenuTrigger = this.mobileService.isMobile();
+    showMenu = !this.showMenuTrigger;
 
-  themeToggle() {
-    this.themeService.themeToggle();
-    this.theme.set(this.themeService.currentTheme);
-  }
+    themeToggle() {
+        this.themeService.themeToggle();
+        this.theme.set(this.themeService.currentTheme);
+    }
 
-  onResize = (event: Event) => {
-    this.showMenuTrigger = this.mobileService.isMobile();
-    this.showMenu = !this.showMenuTrigger;
-  };
+    onResize = (event: Event) => {
+        this.showMenuTrigger = this.mobileService.isMobile();
+        this.showMenu = !this.showMenuTrigger;
+    };
 }
