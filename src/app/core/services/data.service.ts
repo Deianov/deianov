3165839ad4a8d5b29e3d45b763483123cc29912e
Ognian Deianov <44973718+Deianov@users.noreply.github.com>
2024-04-27 20:06:01 +0200

    Update data.service.ts

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -1,21 +1,19 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import adaptPrivateData from '@features/private/private.data.adapter';
-import { IPrivate, IPrivateAdapted } from '@features/private/private.model';
+import {IPrivate, IPrivateAdapted} from '@features/private/private.model';
 import IProject from '@features/projects/project.model';
 
-import { private_data, projects } from '../constants/constants';
+import {private_data, projects} from '../constants/constants';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class DataService {
-  getProjects(): Promise<readonly IProject[]> {
-    return Promise.resolve(projects);
-  }
+    getProjects(): Promise<readonly IProject[]> {
+        return Promise.resolve(projects);
+    }
 
-  getPrivateData(): Promise<IPrivateAdapted> {
-    return Promise.resolve(private_data).then((data: IPrivate) =>
-      adaptPrivateData(data)
-    );
-  }
+    getPrivateData(): Promise<IPrivateAdapted> {
+        return Promise.resolve(private_data).then((data: IPrivate) => adaptPrivateData(data));
+    }
 }
