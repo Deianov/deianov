338b6062167d322d221a076f6905805dd31ce1ad
Ognian Deianov <44973718+Deianov@users.noreply.github.com>
2024-05-03 12:06:30 +0200

    Add a close event to the pop-up menu on mobile.

--- src/app/core/components/nav/nav.component.ts
+++ src/app/core/components/nav/nav.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, model } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
-  selector: 'app-nav',
-  standalone: true,
-  imports: [RouterLink, RouterLinkActive],
-  templateUrl: './nav.component.html',
+    selector: 'app-nav',
+    standalone: true,
+    imports: [RouterLink, RouterLinkActive],
+    templateUrl: './nav.component.html',
+    styleUrl: './nav.component.css',
 })
-export class NavComponent {}
+export class NavComponent {
+    showMenu = model.required<boolean>();
+    showMenuTrigger = model.required<boolean>();
+
+    autoCloseOnMobile() {
+        if (this.showMenuTrigger() && this.showMenu()) {
+            this.showMenu.set(false);
+        }
+    }
+}
