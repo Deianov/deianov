b24e12375d4ea091bb8d4d536c5ebe9eaa89eb1d
Ognian Deianov <44973718+Deianov@users.noreply.github.com>
2024-06-16 17:49:28 +0200

    Experimental zoneless

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,8 @@
-import {ApplicationConfig} from '@angular/core';
-import {provideRouter} from '@angular/router';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
 
-import {routes} from './app.routes';
+import { routes } from './app.routes';
 
-export const appConfig: ApplicationConfig = {providers: [provideRouter(routes)]};
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes), provideExperimentalZonelessChangeDetection()],
+};
